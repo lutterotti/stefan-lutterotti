7fca6b3073878758d8718a158aff85efd807c69b
Stef Lutterotti <luteroti@gmail.com>
2021-04-24 22:56:46 -0600

    remove router

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,7 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
+        <Home />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
