f859c342eed3781ec2749bca5d2ebc1d22f16e1c
lutterotti <luteroti@gmail.com>
2022-01-19 11:27:32 -0700

    small change

--- src/components/home/IconContainer.tsx
+++ src/components/home/IconContainer.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as LogoLinkedIn } from '../../assets/logo-linkedin.svg';
 import { ReactComponent as LogoEmail } from '../../assets/mail-outline.svg';
 import { Link } from 'react-router-dom';
 
-export default function IconContainer () {
+export default function IconContainer() {
   return (
     <div className="icon-container">
       <Link to={{ pathname: "https://github.com/lutterotti" }} target="_blank">
@@ -12,19 +12,19 @@ export default function IconContainer () {
           <svg className="svg"><LogoGithub></LogoGithub></svg>
           <p className="icon-title">lutterotti</p>
         </div>
-        </Link>
-        <Link to={{ pathname: "https://www.linkedin.com/in/stefani-lutterotti" }} target="_blank">
-          <div className="icon" id="linked">
-            <svg className="svg"><LogoLinkedIn></LogoLinkedIn></svg>
-            <p className="icon-title">Stefani Lutterotti</p>
-          </div>
-        </Link>
-        <Link to={{ pathname: "mailto:luteroti@gmail.com" }} target="_blank">
-          <div className="icon" id="email">
-            <svg className="svg"><LogoEmail></LogoEmail></svg>
-            <p className="icon-title">luteroti@gmail.com</p>
-          </div>
-        </Link>
+      </Link>
+      <Link to={{ pathname: "https://www.linkedin.com/in/stefani-lutterotti" }} target="_blank">
+        <div className="icon" id="linked">
+          <svg className="svg"><LogoLinkedIn></LogoLinkedIn></svg>
+          <p className="icon-title">Stefan Lutterotti</p>
+        </div>
+      </Link>
+      <Link to={{ pathname: "mailto:luteroti@gmail.com" }} target="_blank">
+        <div className="icon" id="email">
+          <svg className="svg"><LogoEmail></LogoEmail></svg>
+          <p className="icon-title">luteroti@gmail.com</p>
+        </div>
+      </Link>
     </div>
   )
 }
--- src/components/home/InformationCard.tsx
+++ src/components/home/InformationCard.tsx
@@ -2,18 +2,18 @@ import './InformationCard.scss';
 import IconContainer from './IconContainer';
 import InformationQuote, { Quote } from './InformationQuote';
 import { useEffect, useState } from 'react';
-interface ContainerProps {}
+interface ContainerProps { }
 
 const InformationCard: React.FC<ContainerProps> = () => {
   const [current_index, updateIndex] = useState(0);
   const quotes: Quote[] = [
-    {text: `K-I-S-S, keep it simple stupid. Great advice, hurts my feelings every time.`, author: `Dwight Schrute`},
-    {text: `An office is a place where dreams come true.`, author: `Michael Scott`},
-    {text: `Whenever I'm about to do something, I think, 'Would an idiot do that?' and if they would, I do not do that thing.`, author: `Dwight Schrute`},
-    {text: `Why waste time say lot word when few word do trick?`, author: `Kevin Malone`},
-    {text: `You guys I'm like really smart now. You don't even know. You could ask me, Kelly what's the biggest company in the world? And I'd be like, 'blah blah blah, blah blah blah blah blah blah.' Giving you the exact right answer.`, author: `Kelly Kapoor`},
-    {text: `ACTUALLY`, author: `Oscar Martinez`},
-    {text: 'I am Beyonce always.', author: `Michael Scott`},
+    { text: `K-I-S-S, keep it simple stupid. Great advice, hurts my feelings every time.`, author: `Dwight Schrute` },
+    { text: `An office is a place where dreams come true.`, author: `Michael Scott` },
+    { text: `Whenever I'm about to do something, I think, 'Would an idiot do that?' and if they would, I do not do that thing.`, author: `Dwight Schrute` },
+    { text: `Why waste time say lot word when few word do trick?`, author: `Kevin Malone` },
+    { text: `You guys I'm like really smart now. You don't even know. You could ask me, Kelly what's the biggest company in the world? And I'd be like, 'blah blah blah, blah blah blah blah blah blah.' Giving you the exact right answer.`, author: `Kelly Kapoor` },
+    { text: `ACTUALLY`, author: `Oscar Martinez` },
+    { text: 'I am Beyonce always.', author: `Michael Scott` },
   ]
 
   useEffect(() => {
@@ -23,21 +23,21 @@ const InformationCard: React.FC<ContainerProps> = () => {
     }, 6500)
 
     return () => {
-        clearInterval(interval);
+      clearInterval(interval);
     }
   }, [current_index]);
 
   function displayCurrentQuote(index: number, quote: Quote) {
     if (index === current_index) {
-      return (<div className={ 'quote--display'} key={index}><InformationQuote text={quote.text} author={quote.author} key={index}></InformationQuote></div>);
+      return (<div className={'quote--display'} key={index}><InformationQuote text={quote.text} author={quote.author} key={index}></InformationQuote></div>);
     } else {
-      return (<div className={ 'quote--hide'} key={index}><InformationQuote text={quote.text} author={quote.author} key={index}></InformationQuote></div>);
+      return (<div className={'quote--hide'} key={index}><InformationQuote text={quote.text} author={quote.author} key={index}></InformationQuote></div>);
     }
   }
 
   return (
     <div className="info-card">
-      <h6 className="info-card__title">Stefani Lutterotti</h6>
+      <h6 className="info-card__title">Stefan Lutterotti</h6>
       <h6 className="info-card__sub-title">Front-End Developer</h6>
       <div className="info-card__quote-container">{quotes.map((quote: Quote, index: number) => displayCurrentQuote(index, quote))}</div>
       <IconContainer></IconContainer>
